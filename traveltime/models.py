64d099377ef249aad51feefe9bce2cf0fef05b7f
Rob Owen <r.owen767@gmail.com>
2016-11-19 13:13:53 +0000

    Added database tables

--- traveltime/models.py
+++ traveltime/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# UK locations, modified from Becca's USA work
+class UK_locations(models.Model):
+	country_code = models.CharField(max_length=2)
+	postal_code = models.CharField(max_length=20)
+	place_name = models.CharField(max_length=180, db_index=True)
+	admin_name1 = models.CharField(max_length=100)
+	admin_code1 = models.CharField(max_length=20)
+	admin_name2 = models.CharField(max_length=100)
+	admin_code2 = models.CharField(max_length=20)
+	admin_name3 = models.CharField(max_length=100)
+	admin_code3 = models.CharField(max_length=20)
+	latitude = models.FloatField()
+	longitude = models.FloatField()
+	accuracy = models.IntegerField()
+
+# UK town populations ()
+class Population(models.Model):
+	place_name = models.CharField(max_length=180, db_index=True)
+	pop = models.IntegerField()
--- traveltime/urls.py
+++ traveltime/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from django.http import HttpResponse
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', HttpResponse("Placeholder")),
+    url(r'^$', views.index, name='index'),
 ]
